a30ee11554edb2727b1b901719c323133ca10430
NiloufarRashedi <niloufar.rashedi@gmail.com>
2020-10-20 12:38:57 +0200

    MVVM setup and Calibrun Micro configuratons added

---
+++ TRMWPFUserInterface/ViewModels/ShellViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRMWPFDesktopUserInterface.ViewModels
+{
+    public class ShellViewModel
+    {
+    }
+}
