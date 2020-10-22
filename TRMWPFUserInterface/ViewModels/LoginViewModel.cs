9be1619dd9b9d733318e0d65f64478c3d8025566
NiloufarRashedi <niloufar.rashedi@gmail.com>
2020-10-22 17:21:20 +0200

    Bad Request message fixed

--- TRMWPFUserInterface/ViewModels/LoginViewModel.cs
+++ TRMWPFUserInterface/ViewModels/LoginViewModel.cs
@@ -44,6 +44,34 @@ namespace TRMWPFDesktopUserInterface.ViewModels
 
             }
         }
+
+        public bool IsErrorVisible
+        {
+            get 
+            {
+                bool output = false;
+                if (ErrorMessage?.Length > 0)
+                {
+                    output = true;
+                }
+                return output;
+            }
+        }
+
+        private string _errorMessage;
+
+        public string ErrorMessage
+        {
+            get { return _errorMessage; }
+            set 
+            {
+                _errorMessage = value;
+                NotifyOfPropertyChange(() => IsErrorVisible);
+                NotifyOfPropertyChange(() => ErrorMessage);
+                 
+            }
+        }
+
         public bool CanLogIn/*(string userName, string password)*/
         {
             get
@@ -61,12 +89,16 @@ namespace TRMWPFDesktopUserInterface.ViewModels
         {
             try
             {
+                ErrorMessage = "";
                 var result = await _apiHelper.Authenticate(UserName, Password);
 
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.Message);   
-            }        }
+                //Console.WriteLine(ex.Message); 
+                ErrorMessage = ex.Message;
+
+            }        
+        }
     }
 }
